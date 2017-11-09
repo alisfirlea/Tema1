643381fb3f0a6e3ca4dcabefca42336c0ed7cd70
Sfirlea Alina <alinasfirlea23@yahoo.com>
2017-11-09 17:40:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x=10,y=15;
+        float a=0;
+        a=x+y;
+        a=a/2;
+        System.out.println("Media aritmetica a numerelor este "+a);
+    }
+}
